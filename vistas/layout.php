1f7e5ac8996a72596694e78dd2100822ee597340
vmvanegas8 <vmvanegas8@misena.edu.co>
2021-07-02 14:47:07 -0500

    favicon añadido

--- vistas/layout.php
+++ vistas/layout.php
@@ -17,6 +17,7 @@ if (session_status() === PHP_SESSION_NONE) {
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
   <script src="https://kit.fontawesome.com/ac5e861b74.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="../assets/css/misEstilos.css">
+  <link rel="shortcut icon" href="../assets/pic/php-logo.png" type="image/x-icon">
   <title>Taller PHP</title>
 </head>
 
